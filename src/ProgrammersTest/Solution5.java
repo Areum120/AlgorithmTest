749cb9920a54289d6c23d74c39f4a6149b050f2d
LAPTOP-F3LFLS66\tjfsu <tjfsu120@gmail.com>
2021-03-06 15:25:03 +0900

    문자열 가운데 글자 반환 update

--- src/ProgrammersTest/Solution5.java
+++ src/ProgrammersTest/Solution5.java
@@ -6,46 +6,46 @@ package ProgrammersTest;
   ex) s="abcde", return="c"
 s는 길이가 1 이상, 100이하인 스트링입니다.
 */
-public class Solution5 {
+//	public class Solution5 {
+//	public static String solution(String s) {
+//	
+//	String answer;//가운데 반환 글자
+//	
+//		if(s.length()%2==0) {//가 짝수이면 가운데 2글자 반환 
+//		 answer =s.substring(s.length()/2-1, s.length()/2+1);
+//		//System.out.println(s.substring(s.length()/2-1, s.length()/2+1));//s의 인덱스로 접근하여 substring 몫을 구한 뒤 4/2= 2, 6/2= 3, 즉 몫-1, 몫
+//		}else //홀수이면 가운데 1글자 반환
+//		answer =s.substring(s.length()/2, s.length()/2+1);
+//		//System.out.println(s.charAt(s.length()/2));//chartAt은 String 타입 오류로 substring으로 바꿈
+//		//s의 인덱스로 접근하여 몫을 구한 뒤 +1위치 글자 반환 5/2=2, 7/2=3, 9/2=4
+//
+//	return answer;
+//	
+//	}
+//	
+//	public static void main(String[] args) {
+//		
+//		solution("abcdef");
+//
+//	}
+//}
 
-	public static String solution(String s) {
-	
-	String answer;//가운데 반환 글자
-	
-		if(s.length()%2==0) {//가 짝수이면 가운데 2글자 반환 
-		 answer =s.substring(s.length()/2-1, s.length()/2+1);
-		//System.out.println(s.substring(s.length()/2-1, s.length()/2+1));//s의 인덱스로 접근하여 substring 몫을 구한 뒤 4/2= 2, 6/2= 3, 즉 몫-1, 몫
-		}else //홀수이면 가운데 1글자 반환
-		answer =s.substring(s.length()/2, s.length()/2+1);
-		//System.out.println(s.charAt(s.length()/2));//chartAt은 String 타입 오류로 substring으로 바꿈
-		//s의 인덱스로 접근하여 몫을 구한 뒤 +1위치 글자 반환 5/2=2, 7/2=3, 9/2=4
-
-	return answer;
-	
-	}
-	
-	public static void main(String[] args) {
+	//다른 사람 풀이 
+	class Solution5{
 		
-		solution("abcdef");
-
-	}
-	
-	/*
-	 * 아래는 다른 사람 풀이 참고 
-	 * class StringExercise{
-    String getMiddle(String word){
-
-        return word.substring((word.length()-1) / 2, word.length()/2 + 1);    
+    String getMiddle(String s){
+        return s.substring((s.length()-1)/2, s.length()/2 + 1);
+        //내가 작성한 코드s.length()/2-1과 (s.length()-1)/2는 차이가 있음
     }
     // 아래는 테스트로 출력해 보기 위한 코드입니다.
-    public static void  main(String[] args){
-        StringExercise se = new StringExercise();
-        System.out.println(se.getMiddle("power"));
-    }
-}
-	 * 
-	 */
-}
+    	public static void  main(String[] args){
+    		
+        Solution5 s5 = new Solution5();
+        
+        System.out.println(s5.getMiddle("powerad"));
+    	}
+
+	}
 	
 
 
