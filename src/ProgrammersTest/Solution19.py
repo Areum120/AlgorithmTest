a7af7e8d6eaafb49a8a1bfecd0b985355ea6843b
LAPTOP-F3LFLS66\tjfsu <tjfsu120@gmail.com>
2021-11-26 23:11:39 +0900

    자연수 뒤집어 배열로 만들기

---
+++ src/ProgrammersTest/Solution19.py
@@ -0,0 +1,49 @@
+'''
+자연수 뒤집어 배열로 만들기
+
+Description
+자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
+
+제한 조건
+n은 10,000,000,000이하인 자연수입니다.
+입출력 예
+n	return
+12345	[5,4,3,2,1]
+
+'''
+
+# 문제 1
+# def solution(n):
+#     n_split = list(str(n))
+#     str_answer = n_split.reverse()
+#     print(str_answer)
+# solution(1234)
+# 여기까진 str형으로 거꾸로 출력
+# 여기서 int형으로 바꿔야 만점
+
+
+# 문제2
+def solution(n):
+    # n을 한자리씩 쪼갠다
+    n_split = str(n)
+    # print(len(n_split))
+    print(n_split)
+    arr =[]
+    for i in range(len(n_split)):
+        print(i)
+        print(n_split[i])
+        arr.append(n_split[i])
+    # 쪼갠 n을 뒤에서부터 하나씩 꺼내어 앞자리에 넣는다
+    arr_reverse = arr[::-1]
+    print(arr_reverse)
+    # 쪼갠 n을 다시 int형으로 바꾼다
+    answer = []
+    for j in arr_reverse:
+        j = int(j)
+        print(j)
+        answer.append(j)
+    # 쪼갠 n을 answer에 넣는다
+    print(answer)
+    return answer
+
+solution(1234)
--- src/ProgrammersTest/sum_n.py
+++
@@ -1,9 +0,0 @@
-
-import os
-import pandas as pd
-
-df0 = pd.read_csv('D:\\data\\21.07_BMW미팅\\df_data\\01.2017년_월별_6브랜드_판매량.csv', encoding='utf-8')
-df0
-
-def accumulate(n):
-    df
