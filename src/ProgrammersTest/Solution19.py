8b013d5a98e2b43b995532ce0000e4732a8a615c
LAPTOP-F3LFLS66\tjfsu <tjfsu120@gmail.com>
2021-11-27 11:40:21 +0900

    while문으로 풀기

--- src/ProgrammersTest/Solution19.py
+++ src/ProgrammersTest/Solution19.py
@@ -19,31 +19,45 @@ n	return
 #     print(str_answer)
 # solution(1234)
 # 여기까진 str형으로 거꾸로 출력
-# 여기서 int형으로 바꿔야 만점
+# 여기서 int형으로 바꿔야 통과
 
 
 # 문제2
+# def solution(n):
+#     # n을 한자리씩 쪼갠다
+#     n_split = str(n)
+#     # print(len(n_split))
+#     print(n_split)
+#     arr =[]
+#     for i in range(len(n_split)):
+#         print(i)
+#         print(n_split[i])
+#         arr.append(n_split[i])
+#     # 쪼갠 n을 뒤에서부터 하나씩 꺼내어 앞자리에 넣는다
+#     arr_reverse = arr[::-1]
+#     print(arr_reverse)
+#     # 쪼갠 n을 다시 int형으로 바꾼다
+#     answer = []
+#     for j in arr_reverse:
+#         j = int(j)
+#         print(j)
+#         answer.append(j)
+#     # 쪼갠 n을 answer에 넣는다
+#     print(answer)
+#     return answer
+
+# 문제3
 def solution(n):
-    # n을 한자리씩 쪼갠다
-    n_split = str(n)
-    # print(len(n_split))
-    print(n_split)
-    arr =[]
-    for i in range(len(n_split)):
-        print(i)
-        print(n_split[i])
-        arr.append(n_split[i])
-    # 쪼갠 n을 뒤에서부터 하나씩 꺼내어 앞자리에 넣는다
-    arr_reverse = arr[::-1]
-    print(arr_reverse)
-    # 쪼갠 n을 다시 int형으로 바꾼다
-    answer = []
-    for j in arr_reverse:
-        j = int(j)
-        print(j)
-        answer.append(j)
-    # 쪼갠 n을 answer에 넣는다
-    print(answer)
+    answer=[]
+    while (n>0):
+        # n을 10으로 나눈 나머지
+        reminder = n % 10
+        print(reminder)#4
+        answer.append(reminder)
+        # n을 10으로 나눈 몫
+        n = n // 10
+        print(n)#123
+        # break를 걸면 마지막 글자만 출력됨, 안걸면 계속 반복되므로 전체 숫자를 input
     return answer
 
-solution(1234)
+solution(123456)
